c695f36e522fca1bf4f4a8bb6454712e36d1291c
ryan <ball.ryan.rb@gmail.com>
2021-05-03 15:03:52 -0600

    adds new routes for misc functionality

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,23 @@
 class ExamplePagesController < ApplicationController
+  def say_hello
+    render json: {message: "Hello!", time: Time.now.strftime("%e %b %Y %H:%M:%S%p")}
+  end
+  
+  def ask_question
+    puts "Feelings are good."
+    render json: {message: "How are you doing today?"}
+  end
+
+  def say_goodbye
+    puts "Guess we're done talking."
+    render json: {message: "Great chat! Goodbye."}
+  end
+
+  def link_to_music
+    render json: {music: "https://www.youtube.com/watch?v=m-xqBm2ANoU"}
+  end
+
+  def climbing_routes
+    render json: [{name: "Boulder Quartz System", location: "Boulder Canyon - Plotinus Wall"}, {name: "10 Digit Dialing", location: "Clear Creek Canyon - Wall of the 90s"}]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/hello_path", controller: "example_pages", action: "say_hello"
+  
+  get "/question_path", controller: "example_pages", action: "ask_question"
+  
+  get "/goodbye_path", controller: "example_pages", action: "say_goodbye"
+
+  get "/music_path", controller: "example_pages", action: "link_to_music"
+
+  get "/climbing_routes_path", controller: "example_pages", action: "climbing_routes"
 end
